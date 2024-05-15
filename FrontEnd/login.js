3ed5b5ef16d17b31fe1707257e76189a0e927788
Dijoyce <celton.jordan@outlook.fr>
2024-05-15 10:05:22 +0200

    add function login & logout

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,83 +1,86 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Le DOM est chargé.");
+  const loginForm = document.getElementById("loginForm");
+  const loginLink = document.getElementById("loginLink");
 
-  // Gestionnaire d'événement pour le formulaire de connexion
-  document
-    .getElementById("loginForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      console.log("Soumission du formulaire de connexion.");
-      login();
-    });
-
-  // Fonction de connexion
-  function login() {
-    console.log("Appel de la fonction login");
-    let emailValue = document.getElementById("email").value;
-    let passwordValue = document.getElementById("password").value;
-
-    if (
-      emailValue !== undefined &&
-      passwordValue !== undefined &&
-      isValidEmail(emailValue)
-    ) {
-      const body = { email: emailValue, password: passwordValue };
-      console.log(body);
-      console.log(JSON.stringify(body));
-      fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify({ email: emailValue, password: passwordValue }),
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            alert("Nom utilisateur - mot de passe incorrect.");
-            return Promise.reject("Nom utilisateur - mot de passe incorrect.");
-          }
-        })
-        .then((value) => {
-          localStorage.setItem("isLoggedIn", "true"); // Indique que l'utilisateur est connecté
-          localStorage.setItem("token", value.token); // Stocke le token
-          console.log("Connexion réussie. Redirection vers index.html");
-          window.location.href = "index.html";
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    } else {
-      alert("Nom utilisateur - mot de passe incorrect.");
-    }
-  }
-
-  // Fonction de validation de l'email
   function isValidEmail(email) {
+    // Pattern de l'adresse e-mail
     const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    // Test de l'adresse e-mail avec la regex
+    console.log(pattern.test(email));
     return pattern.test(email);
   }
 
-  // Vérifie si l'utilisateur est connecté
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-
-  // Sélectionne l'élément de navigation pour le bouton de connexion
-  const loginNavItem = document.querySelector("#loginNavItem");
-
-  // Si l'utilisateur est connecté, affiche le bouton de déconnexion
-  if (isLoggedIn === "true") {
-    const logoutLink = document.createElement("a");
-    logoutLink.href = "#";
-    logoutLink.textContent = "logout";
-    logoutLink.addEventListener("click", function () {
-      // Déconnexion de l'utilisateur : supprime le token et marque comme déconnecté
-      localStorage.removeItem("isLoggedIn");
+  // Vérifiez si l'utilisateur est connecté
+  const token = localStorage.getItem("token");
+  if (token) {
+    // Si l'utilisateur est connecté, affichez le bouton de déconnexion
+    loginLink.textContent = "logout";
+    loginLink.href = "#";
+    loginLink.addEventListener("click", function logoutHandler(event) {
+      event.preventDefault();
+      // Logique de déconnexion
       localStorage.removeItem("token");
-      // Recharge la page pour refléter le changement
-      location.reload();
+      alert("Déconnecté!");
+      loginLink.textContent = "login";
+      loginLink.href = "login.html";
+      loginLink.removeEventListener("click", logoutHandler);
     });
-    loginNavItem.innerHTML = ""; // Efface le contenu précédent
-    loginNavItem.appendChild(logoutLink);
+  } else {
+    // Si l'utilisateur n'est pas connecté, affichez le bouton de connexion
+    loginLink.textContent = "login";
+    loginLink.href = "login.html";
+  }
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      if (!isValidEmail(email)) {
+        alert("Veuillez saisir une adresse e-mail valide.");
+        return;
+      }
+
+      const loginData = {
+        email: email,
+        password: password,
+      };
+      fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      })
+        .then((response) => {
+          // console.log("Response status:", response.status);
+          if (!response.ok) {
+            throw new Error(
+              "Network response was not ok " + response.statusText
+            );
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("affichage token:", data);
+          if (data.token) {
+            // Stocker le token
+            localStorage.setItem("token", data.token);
+            alert("Connexion réussie!");
+            window.location.href = "index.html"; // Rediriger vers la page d'accueil après connexion
+          } else {
+            // Échec de la connexion
+            alert(
+              "Erreur de connexion: " + (data.message || "Aucun token reçu")
+            );
+          }
+        })
+        .catch((error) => {
+          console.error("Erreur:", error);
+          alert("Adresse mail ou mot de passe incorrect");
+        });
+    });
   }
 });
