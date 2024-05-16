2767cb85aff07951760d56f83331d30f8034977b
Dijoyce <celton.jordan@outlook.fr>
2024-05-16 15:22:20 +0200

    refactoring code

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,3 +1,81 @@
+// Fonction pour afficher les travaux dans la galerie
+function displayWorks(works) {
+  // Sélectionner la galerie
+  const gallery = document.querySelector(".gallery");
+  // Supprime tous les éléments enfants de la galerie
+  gallery.innerHTML = "";
+  // Parcourir les travaux filtrés et les ajouter à la galerie
+  works.forEach((work) => {
+    // Créer un élément figure pour chaque travail
+    const figureElement = document.createElement("figure");
+
+    // Créer un élément image et définir ses attributs src et alt
+    const imageElement = document.createElement("img");
+    imageElement.src = work.imageUrl;
+    imageElement.alt = work.title;
+
+    // Créer un élément figcaption
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.textContent = work.title;
+
+    // Ajouter l'image et le titre à l'élément figure
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(figcaptionElement);
+
+    // Ajouter l'élément figure à la div gallery
+    gallery.appendChild(figureElement);
+  });
+}
+
+// Fonction pour créer la liste de filtres et l'ajouter avant la galerie
+function createFilterList(data) {
+  // Créer l'élément ul pour la liste de filtres
+  const filterList = document.createElement("ul");
+  filterList.classList.add("filter");
+
+  // Liste des noms de filtres
+  const filterNames = [
+    "Tous",
+    "Objets",
+    "Appartements",
+    "Hotels & restaurants",
+  ];
+
+  // Parcourir les noms de filtres et créer les éléments li correspondants
+  filterNames.forEach((name) => {
+    const filterItem = document.createElement("li");
+    filterItem.textContent = name;
+    filterList.appendChild(filterItem);
+  });
+
+  // Sélectionne la section portfolio
+  const portfolioSection = document.getElementById("portfolio");
+
+  // Sélectionne la classe gallery
+  const gallery = portfolioSection.querySelector(".gallery");
+
+  // Ajout de la liste de filtres avant la galerie
+  portfolioSection.insertBefore(filterList, gallery);
+
+  // Ajout de l'écouteur d'événements au clic à la liste de filtres
+  filterList.addEventListener("click", (event) => {
+    // console.log(event);
+    if (event.target.tagName === "LI") {
+      const categoryName = event.target.textContent;
+      let filteredWorks = [];
+      if (categoryName === "Tous") {
+        filteredWorks = data;
+      } else {
+        filteredWorks = data.filter(
+          (work) => work.category.name === categoryName
+        );
+      }
+      // Appelle de la fonction pour afficher les travaux filtrés
+      displayWorks(filteredWorks);
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Masquer la bannière
   const banner = document.querySelector(".banner");
@@ -7,90 +85,11 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) => {
-      // Sélectionner la galerie
-      const gallery = document.querySelector(".gallery");
-
-      // Fonction pour afficher les travaux dans la galerie
-      function displayWorks(works) {
-        // Supprime tous les éléments enfants de la galerie
-        gallery.innerHTML = "";
-        // Parcourir les travaux filtrés et les ajouter à la galerie
-        works.forEach((work) => {
-          // Créer un élément figure pour chaque travail
-          const figureElement = document.createElement("figure");
-
-          // Créer un élément image et définir ses attributs src et alt
-          const imageElement = document.createElement("img");
-          imageElement.src = work.imageUrl;
-          imageElement.alt = work.title;
-
-          // Créer un élément figcaption
-          const figcaptionElement = document.createElement("figcaption");
-          figcaptionElement.textContent = work.title;
-
-          // Ajouter l'image et le titre à l'élément figure
-          figureElement.appendChild(imageElement);
-          figureElement.appendChild(figcaptionElement);
-
-          // Ajouter l'élément figure à la div gallery
-          gallery.appendChild(figureElement);
-        });
-      }
-
-      // Fonction pour créer la liste de filtres et l'ajouter avant la galerie
-      function createFilterList() {
-        // Créer l'élément ul pour la liste de filtres
-        const filterList = document.createElement("ul");
-        filterList.classList.add("filter");
-
-        // Liste des noms de filtres
-        const filterNames = [
-          "Tous",
-          "Objets",
-          "Appartements",
-          "Hotels & restaurants",
-        ];
-
-        // Parcourir les noms de filtres et créer les éléments li correspondants
-        filterNames.forEach((name) => {
-          const filterItem = document.createElement("li");
-          filterItem.textContent = name;
-          filterList.appendChild(filterItem);
-        });
-
-        // Sélectionne la section portfolio
-        const portfolioSection = document.getElementById("portfolio");
-
-        // Sélectionne la classe gallery
-        const gallery = portfolioSection.querySelector(".gallery");
-
-        // Ajout de la liste de filtres avant la galerie
-        portfolioSection.insertBefore(filterList, gallery);
-
-        // Ajout de l'écouteur d'événements au clic à la liste de filtres
-        filterList.addEventListener("click", (event) => {
-          // console.log(event);
-          if (event.target.tagName === "LI") {
-            const categoryName = event.target.textContent;
-            let filteredWorks = [];
-            if (categoryName === "Tous") {
-              filteredWorks = data;
-            } else {
-              filteredWorks = data.filter(
-                (work) => work.category.name === categoryName
-              );
-            }
-            // Appelle de la fonction pour afficher les travaux filtrés
-            displayWorks(filteredWorks);
-          }
-        });
-      }
-
       // Vérifier si l'utilisateur est connecté avant de créer la liste de filtres
       const token = localStorage.getItem("token");
       if (!token) {
         // Si l'utilisateur n'est pas connecté, appelle de la fonction pour créer la liste de filtres
-        createFilterList();
+        createFilterList(data);
       }
 
       // Appelle de la fonction pour afficher les travaux
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,107 +1,118 @@
+function isValidEmail(email) {
+  // Pattern de l'adresse e-mail
+  const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  // Test de l'adresse e-mail avec la regex
+  console.log(pattern.test(email));
+  return pattern.test(email);
+}
+
+function showBanner() {
+  const banner = document.querySelector(".banner");
+  banner.style.display = "";
+}
+
+function hideBanner() {
+  const banner = document.querySelector(".banner");
+  banner.style.display = "none";
+}
+
+function logoutHandler(event) {
+  const loginLink = document.getElementById("loginLink");
+  event.preventDefault();
+  // Logique de déconnexion
+  localStorage.removeItem("token");
+  alert("Déconnecté!");
+  loginLink.textContent = "login";
+  loginLink.href = "login.html";
+  loginLink.removeEventListener("click", logoutHandler);
+  hideBanner();
+
+  // Réinitialiser la liste de filtres en supprimant l'élément <ul> de la section portfolio
+  const filterList = document.querySelector(".filter");
+  if (filterList) {
+    filterList.parentNode.removeChild(filterList);
+  }
+
+  // Recharger la page pour réafficher la liste de travaux
+  window.location.reload();
+}
+function showbuttonlogout() {
+  const loginLink = document.getElementById("loginLink");
+  // Si l'utilisateur est connecté, affichez le bouton de déconnexion
+  loginLink.textContent = "logout";
+  loginLink.href = "#";
+  loginLink.addEventListener("click", logoutHandler);
+}
+
+function showbuttonlogin() {
+  const loginLink = document.getElementById("loginLink");
+  loginLink.textContent = "login";
+  loginLink.href = "login.html";
+}
+
+function sendEmailAndPassword(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const loginData = {
+    email: email,
+    password: password,
+  };
+
+  if (!isValidEmail(email)) {
+    alert("Veuillez saisir une adresse e-mail valide.");
+    return;
+  }
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  })
+    .then((response) => {
+      // console.log("Response status:", response.status);
+      if (!response.ok) {
+        throw new Error("Erreur réponse réseau " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("affichage token:", data);
+      if (data.token) {
+        // Stocker le token
+        localStorage.setItem("token", data.token);
+        alert("Connexion réussie!");
+        window.location.href = "index.html"; // Rediriger vers la page d'accueil après connexion
+      } else {
+        // Échec de la connexion
+        alert("Erreur de connexion: " + (data.message || "Aucun token reçu"));
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur:", error);
+      alert("Adresse mail ou mot de passe incorrect");
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
   const loginLink = document.getElementById("loginLink");
   const banner = document.querySelector(".banner");
 
-  function isValidEmail(email) {
-    // Pattern de l'adresse e-mail
-    const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    // Test de l'adresse e-mail avec la regex
-    console.log(pattern.test(email));
-    return pattern.test(email);
-  }
-
-  function showBanner() {
-    banner.style.display = "block";
-  }
-
-  function hideBanner() {
-    banner.style.display = "none";
-  }
-
   // Vérifiez si l'utilisateur est connecté
-  const token = localStorage.getItem("token");
-  if (token) {
-    // Si l'utilisateur est connecté, affichez le bouton de déconnexion
-    loginLink.textContent = "logout";
-    loginLink.href = "#";
-    loginLink.addEventListener("click", function logoutHandler(event) {
-      event.preventDefault();
-      // Logique de déconnexion
-      localStorage.removeItem("token");
-      alert("Déconnecté!");
-      loginLink.textContent = "login";
-      loginLink.href = "login.html";
-      loginLink.removeEventListener("click", logoutHandler);
-      // Cacher la bannière après la déconnexion
-      hideBanner();
-
-      // Réinitialiser la liste de filtres en supprimant l'élément <ul> de la section portfolio
-      const filterList = document.querySelector(".filter");
-      if (filterList) {
-        filterList.parentNode.removeChild(filterList);
-      }
-
-      // Recharger la page pour réafficher la liste de travaux
-      window.location.reload();
-    });
-
+  const connected = localStorage.getItem("token");
+  if (connected) {
     // Afficher la bannière
+    showbuttonlogout();
     showBanner();
   } else {
     // Si l'utilisateur n'est pas connecté, affichez le bouton de connexion
-    loginLink.textContent = "login";
-    loginLink.href = "login.html";
-  }
-
-  if (loginForm) {
-    loginForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-
-      if (!isValidEmail(email)) {
-        alert("Veuillez saisir une adresse e-mail valide.");
-        return;
-      }
-
-      const loginData = {
-        email: email,
-        password: password,
-      };
-      fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(loginData),
-      })
-        .then((response) => {
-          // console.log("Response status:", response.status);
-          if (!response.ok) {
-            throw new Error("Erreur réponse réseau " + response.statusText);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log("affichage token:", data);
-          if (data.token) {
-            // Stocker le token
-            localStorage.setItem("token", data.token);
-            alert("Connexion réussie!");
-            window.location.href = "index.html"; // Rediriger vers la page d'accueil après connexion
-          } else {
-            // Échec de la connexion
-            alert(
-              "Erreur de connexion: " + (data.message || "Aucun token reçu")
-            );
-          }
-        })
-        .catch((error) => {
-          console.error("Erreur:", error);
-          alert("Adresse mail ou mot de passe incorrect");
-        });
-    });
+    showbuttonlogin();
+    if (loginForm) {
+      loginForm.addEventListener("submit", sendEmailAndPassword);
+    }
   }
 });
